e529d9e38fb5cd25f407764a29c931d702187fca
Sergio Onischenko <Onishhenko@gmail.com>
2016-03-20 16:25:48 +0200

    Console text

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -12,6 +12,13 @@ namespace Snake
         public int y;
         public char sym;
 
+        public Point()
+        {
+            Console.WriteLine("Создается новая точка");
+        }
+
+      
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
