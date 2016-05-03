6b7e22c36f121bb9c041bbd8f0d253c24e642b55
Sergio Onischenko <Onishhenko@gmail.com>
2016-05-03 14:36:26 +0300

    add new class VerticalLine

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,14 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize( 80, 25 );
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(1, 23, 0, '+');
+            VerticalLine rightLine = new VerticalLine(1, 23, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
+            Point p = new Point(4, 5, '#');
+            p.Draw();
             
             Console.ReadLine();
         }
